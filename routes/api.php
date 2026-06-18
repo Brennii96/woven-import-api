3d3ecf2bb0e9fc1160a20837a24a489b42e380d5
brendan <16559757+Brennii96@users.noreply.github.com>
2026-06-18 21:19:23 +0100

    rework the routes so they're a little clearer about what's returned

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,25 @@ use App\Http\Controllers\InvestorController;
 use App\Http\Controllers\InvestorStatisticsController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/investors/import', ImportInvestorsController::class);
-Route::get('/investors', [InvestorController::class, 'index']);
-Route::get('/investors/export/{format}', [InvestorController::class, 'export']);
+Route::prefix('investors')
+    ->name('investors.')
+    ->group(function (): void {
+        Route::post('/import', ImportInvestorsController::class)->name('imports.store');
+        Route::get('/', [InvestorController::class, 'index'])->name('index');
+        Route::get('/exports/{format}', [InvestorController::class, 'export'])->name('exports.show');
 
-Route::prefix('statistics')
+        Route::prefix('statistics')
+            ->name('statistics.')
+            ->controller(InvestorStatisticsController::class)
+            ->group(function (): void {
+                Route::get('/average-age', 'averageAge')->name('average-age');
+            });
+    });
+
+Route::prefix('investments/statistics')
+    ->name('investments.statistics.')
     ->controller(InvestorStatisticsController::class)
     ->group(function (): void {
-        Route::get('/average-age', 'averageAge');
-        Route::get('/average-amount', 'averageInvestmentAmount');
-        Route::get('/total-investments', 'totalInvestments');
+        Route::get('/average-amount', 'averageInvestmentAmount')->name('average-amount');
+        Route::get('/total', 'totalInvestments')->name('total');
     });
